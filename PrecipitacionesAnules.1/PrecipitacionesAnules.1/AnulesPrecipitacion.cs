d0465dc2e0b2647a2899963d0e3eaa55ed69560b
JesusBorgess <23470139@campeche.tecnm.mx>
2024-06-04 21:40:45 -0600

    se agregael proyecto

---
+++ PrecipitacionesAnules.1/PrecipitacionesAnules.1/AnulesPrecipitacion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecipitacionesAnules._1
+{
+    internal class AnulesPrecipitacion
+    {
+        public class Mes
+        {
+            public int Numero { get; set; }
+            public string Nombre { get; set; }
+
+            public Mes(int numero, string nombre)
+            {
+                Numero = numero;
+                Nombre = nombre;
+            }
+        }
+
+        public class Precipitacion
+        {
+            public Mes Mes { get; set; }
+            public double Valor { get; set; }
+
+            public Precipitacion(Mes mes, double valor)
+            {
+                Mes = mes;
+                Valor = valor;
+            }
+        }
+
+        public class ListaDePrecipitaciones
+        {
+            public int Año { get; set; }
+            public List<Precipitacion> Precipitaciones { get; set; }
+
+            public ListaDePrecipitaciones(int año)
+            {
+                Año = año;
+                Precipitaciones = new List<Precipitacion>();
+            }
+
+            public void AgregarPrecipitacion(Precipitacion precipitacion)
+            {
+                Precipitaciones.Add(precipitacion);
+            }
+
+            public double CalcularPromedioAnual()
+            {
+                double suma = 0;
+                foreach (var precipitacion in Precipitaciones)
+                {
+                    suma += precipitacion.Valor;
+                }
+                return suma / Precipitaciones.Count;
+            }
+
+            public void MostrarVariaciones(double promedioAnual)
+            {
+                Console.WriteLine($"Precipitación promedio anual: {promedioAnual:F2} mm.");
+                Console.WriteLine("Mes\tPromedio\tVariación");
+                foreach (var precipitacion in Precipitaciones)
+                {
+                    double variacion = Math.Abs(precipitacion.Valor - promedioAnual);
+                    Console.WriteLine($"{precipitacion.Mes.Nombre}\t{precipitacion.Valor:F2}\t{variacion:F2}");
+                }
+            }
+        }
+    }
+}
---
+++ PrecipitacionesAnules.1/PrecipitacionesAnules.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using static PrecipitacionesAnules._1.AnulesPrecipitacion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el año:");
+        int año = int.Parse(Console.ReadLine());
+
+        ListaDePrecipitaciones listaDePrecipitaciones = new ListaDePrecipitaciones(año);
+
+        Console.WriteLine("Ingrese las precipitaciones promedio mensuales:");
+        for (int mes = 1; mes <= 12; mes++)
+        {
+            Console.WriteLine($"Mes {mes}:");
+            double valor = double.Parse(Console.ReadLine());
+
+            Mes mesObj = new Mes(mes, GetMesNombre(mes));
+            Precipitacion precipitacion = new Precipitacion(mesObj, valor);
+            listaDePrecipitaciones.AgregarPrecipitacion(precipitacion);
+        }
+
+        double promedioAnual = listaDePrecipitaciones.CalcularPromedioAnual();
+        listaDePrecipitaciones.MostrarVariaciones(promedioAnual);
+
+        Console.ReadKey();
+    }
+
+    static string GetMesNombre(int mes)
+    {
+        switch (mes)
+        {
+            case 1: return "Enero";
+            case 2: return "Febrero";
+            case 3: return "Marzo";
+            case 4: return "Abril";
+            case 5: return "Mayo";
+            case 6: return "Junio";
+            case 7: return "Julio";
+            case 8: return "Agosto";
+            case 9: return "Septiembre";
+            case 10: return "Octubre";
+            case 11: return "Noviembre";
+            case 12: return "Diciembre";
+            default: return "";
+        }
+    }
+}
